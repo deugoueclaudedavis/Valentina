a52439830b765f2c02c0365cd5637015e7ff0ced
D2 <deugoueclaudedaurian@gmail.com>
2026-02-05 04:59:20 +0100

    first send

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ValentinePage from './valentine.js'; // Assurez-vous que le chemin est bon
+
+function App() {
+  return (
+    <div className="App">
+      <ValentinePage />
+    </div>
+  );
+}
+
+export default App;
